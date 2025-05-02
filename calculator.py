2b48a4216ffe3de6e182336dd4dc94a56a0a422c
unknown <universityworkonly02@gmail.com>
2025-05-02 11:25:59 +0500

    Added division functionality

---
+++ calculator.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Division:
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform division using the function
+result = divide_numbers(num1, num2)
+print("The result is:", result)
