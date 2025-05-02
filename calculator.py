357bb2ef1a2269973f5ae1fd1a88c690eb972c6a
unknown <universityworkonly02@gmail.com>
2025-05-02 11:19:02 +0500

    Added subtraction functionality

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+Subtraction:
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+difference = num1 - num2
+print("The difference is:", difference)
